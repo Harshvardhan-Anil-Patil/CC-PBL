04708f00e30360c8a852174ac0176e0506429921
Harshvardhan-Anil-Patil <86935786+Harshvardhan-Anil-Patil@users.noreply.github.com>
2023-05-11 18:29:42 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome All new Java App !";
+		return "Welcome all New Java App !";
 	}
 
 //	echo "# github-actions-example" >> README.md
